72473112e108bdd479bd96189fee07da4dde03b6
GoldenStack <git@goldenstack.net>
2024-12-08 03:12:54 -0600

    Add database setup

---
+++ database_setup.sql
@@ -0,0 +1,16 @@
+-- Set up the database for PDForum
+
+-- Create a user and a database
+CREATE ROLE pdforum LOGIN PASSWORD 'secret';
+CREATE DATABASE pdforum ENCODING 'UTF8' OWNER pdforum;
+
+-- Create the table for users
+CREATE TABLE "users" (
+    username   TEXT UNIQUE NOT NULL,
+    
+    bio        TEXT NOT NULL DEFAULT '',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    password   BYTEA NOT NULL,
+    salt       BYTEA NOT NULL
+);
